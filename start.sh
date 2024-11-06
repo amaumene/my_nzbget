817ff7cb4be8015be72bad9c214cf5bda41de54b
amaumene <amaumene@users.noreply.github.com>
2024-11-06 17:51:55 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ if test ! -f "/config/nzbget.conf"; then
   cp /app/nzbget.conf /config/nzbget.conf
 fi
 
-/app/nzbget -s -o OutputMode=log -c /config/nzbget.conf &
+/app/nzbget -s -o OutputMode=log -c /config/nzbget.conf
